be5275efaba9370a893d14e2680890df79562538
Jodu555 <Jodu505@gmail.com>
2021-08-21 06:10:36 +0200

    Added default cookie usage to the app object

--- jwork/App.js
+++ jwork/App.js
@@ -18,10 +18,12 @@ class App {
         //     view.initFromApp();
         // });
         window.app = this;
-        this.handleRouting();
-        window.addEventListener('hashchange', (event) => {
+        if (this.router) {
             this.handleRouting();
-        });
+            window.addEventListener('hashchange', (event) => {
+                this.handleRouting();
+            });
+        }
     }
 
     handleRouting() {
@@ -96,6 +98,33 @@ class App {
         template.setAttribute('data-load-component', 'index')
         this.element.appendChild(template);
         return template;
+    }
+
+    setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    deleteCookie(cname) {
+        setCookie(cname, -1, -1);
     }
 
 }
